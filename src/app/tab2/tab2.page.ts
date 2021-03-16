c63469da90c64eac6578808313246b348d352988
alex <michaelalexanderrustan@gmail.com>
2021-03-16 21:35:41 +0800

    Commit Soal ke 3

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  angka : number;
+  random = 0;
+  benar : boolean;
   constructor() {}
-
+  check(){
+    
+    this.random = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+    alert(this.random)
+    if(this.angka == this.random){
+      this.benar = true;
+    }else{
+      this.benar = false;
+    }
+  }
 }
